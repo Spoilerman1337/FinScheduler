29e6167c735f550a51c89ce065e0245cfed5e335
Maxim Toropygin <mrcatman1337@gmail.com>
2026-04-25 00:01:15 +0300

    toaster fix

--- finscheduler-web/src/layout/main/Main.tsx
+++ finscheduler-web/src/layout/main/Main.tsx
@@ -3,6 +3,7 @@ import {motion} from "framer-motion";
 import {Route, Routes, matchPath, useLocation} from "react-router-dom";
 import Layout from "./subcomponents/Layout.tsx";
 import {routes} from "./routes.tsx";
+import {Toaster} from "../../components/ui/toaster.tsx";
 
 const MotionFlex = motion.create(Flex)
 
@@ -11,27 +12,30 @@ export default function Main() {
     const currentRoute = routes.find((route) => matchPath({path: route.path, end: true}, location.pathname));
 
     return (
-        <MotionFlex layout
-                    flex={1}
-                    h="100%"
-                    overflowY="auto"
-                    overflowX="hidden"
-                    pl="4"
-                    pr="4"
-                    pt="4"
-                    pb="0"
-                    direction="column"
-                    backdropFilter="blur(8px)"
-                    bg="rgba(0,0,0,0.25)"
-                    minHeight={0}
-                    className="custom-scrollbar">
-            <Layout headerName={currentRoute?.title}>
-                <Routes>
-                    {routes.map((route) => (
-                        <Route key={route.path} path={route.path} element={route.element}/>
-                    ))}
-                </Routes>
-            </Layout>
-        </MotionFlex>
+        <>
+            <MotionFlex layout
+                        flex={1}
+                        h="100%"
+                        overflowY="auto"
+                        overflowX="hidden"
+                        pl="4"
+                        pr="4"
+                        pt="4"
+                        pb="0"
+                        direction="column"
+                        backdropFilter="blur(8px)"
+                        bg="rgba(0,0,0,0.25)"
+                        minHeight={0}
+                        className="custom-scrollbar">
+                <Layout headerName={currentRoute?.title}>
+                    <Routes>
+                        {routes.map((route) => (
+                            <Route key={route.path} path={route.path} element={route.element}/>
+                        ))}
+                    </Routes>
+                </Layout>
+            </MotionFlex>
+            <Toaster />
+        </>
     )
 }
